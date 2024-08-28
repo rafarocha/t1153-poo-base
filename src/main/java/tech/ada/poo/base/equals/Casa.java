965d9f74e98c57285939466aa59015b5c8e80e9d
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2024-08-28 20:42:09 -0300

    exemplos metodo equals

---
+++ src/main/java/tech/ada/poo/base/equals/Casa.java
@@ -0,0 +1,56 @@
+package tech.ada.poo.base.equals;
+
+public class Casa {
+
+    private String numero;
+
+    private Integer complemento; // inteiro de proposito
+
+    public Casa(String num) {
+        this.numero = num;
+    }
+
+    public Casa(String num, Integer complemento) {
+        this.numero = num;
+        this.complemento = complemento;
+        // se nao aceitar zero a hora de reclamar é aqui - exception
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Integer getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(Integer complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Casa) ) return false;
+        if (this.numero == null || this.complemento == null) return false;
+
+        // fazer casting
+        Casa casa = (Casa) obj;
+
+        // comparar com this os atributos
+        boolean r1 = (
+            this.getNumero().equals(casa.getNumero())
+        );
+        boolean r2 = (this.getComplemento().equals(
+            casa.getComplemento())
+        );
+        System.out.println("debug");
+        return (r1 && r2); // tautologia = true
+    }
+
+    // TODO hashcode
+
+}
---
+++ src/main/java/tech/ada/poo/base/equals/MainEquals.java
@@ -0,0 +1,50 @@
+package tech.ada.poo.base.equals;
+
+public class MainEquals {
+
+    public static void main(String[] args) {
+
+        Casa c1 = new Casa("123");
+        c1.setNumero( c1.getNumero() + "abc" );
+        Casa c2 = new Casa("123");
+        c2.setNumero( c2.getNumero() + "abc" );
+
+        boolean iguais = ( c1.equals(c2) );
+        System.out.println( iguais );
+
+        // 1. verificar com ==
+        System.out.println("--1--");
+        // sem a mutacao do setNumero retorna true
+        boolean iguais2 = (c1.getNumero() == c2.getNumero());
+        System.out.println( iguais2 );
+        System.out.println( c1.getNumero() );
+        System.out.println( c2.getNumero() );
+
+        // 2. verificar com equal no numero
+        System.out.println("--2--");
+        boolean iguais3 = (c1.getNumero().equals(c2.getNumero()));
+        System.out.println( iguais3 );
+
+        // 3. verificar com equals com override
+        // POO = ( estado + comportamento )
+        // se nao for assim, vai ter propriedade + funcoes
+        System.out.println("--3--");
+        boolean iguais4 = (c1.equals(c2));
+        System.out.println( iguais4 );
+        Casa c3 = new Casa(null);
+        boolean iguais5 = (c3.equals(c1));
+        System.out.println( iguais5 );
+
+        // 4. verificar multi atributos
+        /// aqui digamos que a regra mudou
+        System.out.println("--4--");
+        Casa c4 = new Casa("987", 1);
+        Casa c5 = new Casa("987", 1);
+        boolean iguais6 = (c4.equals(c5));
+        System.out.println( iguais5 );
+
+
+
+    }
+
+}

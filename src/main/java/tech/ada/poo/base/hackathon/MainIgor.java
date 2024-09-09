111be048d0a30c878fa67bb4b4fbf27d995139d0
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2024-09-09 20:31:50 -0300

    respondendo duvida igor

---
+++ src/main/java/tech/ada/poo/base/hackathon/MainIgor.java
@@ -0,0 +1,12 @@
+package tech.ada.poo.base.hackathon;
+
+public class MainIgor {
+
+    public static void main(String[] args) {
+
+        // visao bottom-up - de baixo para cima
+        // (banco + repositorio) -> (servico + regras) -> (controlador -> view)
+
+    }
+
+}
